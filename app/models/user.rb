77c67f91bf5e93500745b6a1a37a1723ab4d5845
大森優太 <>
2018-10-19 21:39:55 +0900

    edit user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
-         :confirmable, :omniauthable, omniauth_providers: [:facebook]
+         :confirmable, :omniauthable
 
   validates :fullname, presence: true, length: { maximum: 50 }
   include Gravtastic
