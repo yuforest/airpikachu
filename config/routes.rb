f8e483a88f0775a0559ab088a74ddfcc88f4aca2
大森優太 <>
2018-10-19 20:19:13 +0900

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  devise_for :users,
+             path: '',
+             path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+             controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+end
